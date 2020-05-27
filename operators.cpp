d8b4423b6ec66b26672b844fcf42015fefe12299
manishmeshram51 <manishmeshram51@gmail.com>
2020-05-28 02:44:57 +0530

    learning cpp

---
+++ operators.cpp
@@ -0,0 +1,23 @@
+//  arithmatic operators for 2 numbers 2 nd program
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /* code */
+    int firstNum,secondNum;
+    cout<<"Enter first number:"<<endl;
+    cin>>firstNum;
+
+    cout<<"Enter first number: "<<endl;
+    cin>>secondNum;
+
+    cout<<"The sum of 2 numbers is :"<<firstNum + secondNum<<endl;
+    cout<<"The diff of 2 numbers is :"<<firstNum - secondNum<<endl;
+    cout<<"The multi of 2 numbers is :"<<firstNum * secondNum<<endl;
+    cout<<"The div of 2 numbers is :"<<firstNum / secondNum<<endl;
+
+
+    return 0;
+}
